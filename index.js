bf8c2c4ac1b7dd780c2e465e91ff1366dc02a5e6
Chetan Bhagwatkar <101045827+chetanbhagwatkar@users.noreply.github.com>
2024-08-31 16:48:31 +0530

    first DOM mini Project
    player hitting  the ball, and ball showing its direction

---
+++ index.js
@@ -0,0 +1,23 @@
+
+/*
+
+D O M  :  DOCUEMENT OBJECT MODEL , The HTML DOM model is constructed as a tree of Objects:
+ its use to make website interactive with users   
+
+DOM represent as a   docuement
+
+*/
+
+    function changeBtn(){
+
+        document.getElementById("firstimg").src="hit.jpg"
+        document.getElementById("secondimg").src="ball2.jpg"
+        
+        
+    }
+    function stableBtn() {
+        
+        document.getElementById("firstimg").src = "stable.jpg"
+        document.getElementById("secondimg").src="ball.jpg"
+    }
+    
